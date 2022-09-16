32a2f2ed67da8b2110d53a29cf5023127e41e211
Sam <scoulon@villanova.edu>
2022-09-16 15:52:44 -0400

    [WIP] Adjusted to count to N_SIZE-1

--- pqc_accelerator_new/design/counter.vhd
+++ pqc_accelerator_new/design/counter.vhd
@@ -20,7 +20,7 @@ architecture rtl of counter is
 
 begin
 
-    count_nxt <= count + "00000001" when count < "11111111"
+    count_nxt <= count + "00000001" when count < N_SIZE-1
                  else "00000000";
 
     process (clk, rst, ena) begin
