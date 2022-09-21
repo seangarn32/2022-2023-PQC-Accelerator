9ccd08928bb4e80c842c08b3a526ab374e05f51e
Sam <scoulon@villanova.edu>
2022-09-21 17:17:57 -0400

    [WIP] Set N_SIZE to 8

--- pqc_pipe/design/globals_pkg.vhd
+++ pqc_pipe/design/globals_pkg.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_unsigned.all;
 
 package globals_pkg is
 
-    constant N_SIZE : integer := 256;
+    constant N_SIZE : integer := 8;
 
     type a_matrix is array (0 to N_SIZE-1) of std_logic_vector(1 downto 0);
     type b_matrix is array (0 to N_SIZE-1) of std_logic_vector(7 downto 0);
