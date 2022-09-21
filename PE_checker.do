ac24e48b280152d17a4075aa321d17095c38a9dd
Sean Garner <seangarn32@gmail.com>
2022-09-21 19:47:24 -0400

    Python files.

--- PE_checker.do
+++ PE_checker.do
@@ -1,10 +1,9 @@
-restart
-force clock 0 0, 1 5ns -repeat 10ns
-force reset 1 0ns, 0 10ns
-force load 0 0ns, 1 10ns
-force clear 0 0ns
+restart -f
+force clk 0 0, 1 5ns -repeat 10ns
+force rst 1 0ns, 0 10ns
+force ena 0 0ns, 1 10ns
 
-force A_in 1 20ns, 1 30ns, 1 40ns, 0 50ns, 1 60ns, 1 70ns, 1 80ns, 1 90ns
-force B_in 00110010 20ns, 01111110 30ns, 01000111 40ns, 00010100 50ns, 01000111 60ns, 01001011 70ns, 01010001 80ns, 01101110 90ns
+force A_in 00110100 20 ns
+force B_in "01111101" 20 ns, "01010100" 40 ns, "01011100" 60 ns, "01101000" 80 ns, "00111100" 100 ns, "00001111" 120 ns, "01011111" 140 ns, "01110010" 160 ns
 
 run 700000ns
--- pqc_accelerator_new/Python Sims/Senior Project/PE_checker_do_maker.py
+++ pqc_accelerator_new/Python Sims/Senior Project/PE_checker_do_maker.py
@@ -1,15 +1,14 @@
 import numpy as np
 
 N = 8
-
+T = 20
 def createFile(A, B, filepath):
     file = open(filepath, 'w')
     try:
-        file.write("restart\n")
-        file.write("force clock 0 0, 1 5ns -repeat 10ns\n")
-        file.write("force reset 1 0ns, 0 10ns\n")
-        file.write("force load 0 0ns, 1 10ns\n")
-        file.write("force clear 0 0ns\n")
+        file.write("restart -f\n")
+        file.write("force clk 0 0, 1 5ns -repeat 10ns\n")
+        file.write("force rst 1 0ns, 0 10ns\n")
+        file.write("force ena 0 0ns, 1 10ns\n")
         for i in range(0, 2):
             if i == 0:
                 file.write("\nforce A_in ")
@@ -17,17 +16,17 @@ def createFile(A, B, filepath):
                 file.write("\nforce B_in ")
             for j in range(0, len(A)):
                 if i == 0:
-                    num = np.binary_repr(int(A[N-1][j]), width=1)
+                    num = ""
+                    for k in A[0]:
+                        num += str(k)
                     if j == 0:
-                        file.write(num+" "+str(10*(j+1)+10)+"ns")
-                    else:
-                        file.write(", "+num+" "+str(10*(j+1)+10)+"ns")
+                        file.write(num+" "+str(T*(j+1))+" ns")
                 else:
                     num = np.binary_repr(int(B[j][0]), width=8)
                     if j == 0:
-                        file.write(num+" "+str(10*(j+1)+10)+"ns")
+                        file.write("\""+num+"\" "+str(T*(j+1))+" ns")
                     else:
-                        file.write(", "+num+" "+str(10*(j+1)+10)+"ns")
+                        file.write(", \""+num+"\" "+str(T*(j+1))+" ns")
 
         file.write("\n\nrun 700000ns")
                 
