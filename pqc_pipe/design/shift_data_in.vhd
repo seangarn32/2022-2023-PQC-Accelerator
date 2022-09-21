46a27849e8ee8c0a87138336771ad2018a5b0bea
Sam <scoulon@villanova.edu>
2022-09-21 17:17:08 -0400

    [WIP] Added 1bit shift in for A values

--- pqc_pipe/design/shift_data_in.vhd
+++ pqc_pipe/design/shift_data_in.vhd
@@ -8,38 +8,63 @@ entity data_shift_in is
         clk     : in    std_logic;
         rst     : in    std_logic;
         ena     : in    std_logic;
-        d_in    : in    std_logic_vector(7 downto 0);
 
-        d_out   : out   b_matrix
+        a_in    : in    std_logic;
+        b_in    : in    std_logic_vector(7 downto 0);
+
+        a_out   : out   std_logic_vector(N_SIZE-1 downto 0);
+        b_out   : out   b_matrix
     );
 end entity;
 
 architecture rtl of data_shift_in is
 
-    signal d       : b_matrix;
+    signal a       : std_logic_vector(N_SIZE-1 downto 0);
+    signal b       : b_matrix;
 
 begin
 
-    REG_0 : entity work.reg_8bit(rtl)
+    REG_1_0 : entity work.reg_1bit(rtl)
         port map(
             clk,
             rst,
             ena,
-            d_in,
-            d(0)
+            a_in,
+            a(0)
         );
 
-    REG_GEN : for i in 1 to N_SIZE-1 generate
-        REG : entity work.reg_8bit(rtl)
+    REG_1_GEN : for i in 1 to N_SIZE-1 generate
+        REG_1 : entity work.reg_1bit(rtl)
             port map(
                 clk,
                 rst,
                 ena,
-                d(i-1),
-                d(i)
+                a(i-1),
+                a(i)
             );
-    end generate REG_GEN;
+    end generate REG_1_GEN;
 
-    d_out <= d;
+    REG_8_0 : entity work.reg_8bit(rtl)
+        port map(
+            clk,
+            rst,
+            ena,
+            b_in,
+            b(0)
+        );
+
+    REG_8_GEN : for i in 1 to N_SIZE-1 generate
+        REG_8 : entity work.reg_8bit(rtl)
+            port map(
+                clk,
+                rst,
+                ena,
+                b(i-1),
+                b(i)
+            );
+    end generate REG_8_GEN;
+
+    a_out <= a;
+    b_out <= b;
 
 end rtl;
