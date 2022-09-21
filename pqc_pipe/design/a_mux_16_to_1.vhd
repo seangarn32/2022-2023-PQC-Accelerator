99fb148936bc23aa4fc6cd8432e31a94f617b980
Robert Rotyliano <rrotylia@villanova.edu>
2022-09-21 19:18:57 -0400

    fixed script

--- pqc_pipe/design/a_mux_16_to_1.vhd
+++ pqc_pipe/design/a_mux_16_to_1.vhd
@@ -6,7 +6,7 @@ use work.globals_pkg.all;
 entity counter is
     port(
         A_in     : in    a_mux_input;
-        sel      : in    std_logic_vector(3 downto 0);
+        sel      : in    std_logic_vector(N_SIZE/DIVIDE downto 0);
 
         A_out    : out   std_logic_vector(N-1 downto 0)
     );
@@ -15,14 +15,10 @@ end entity;
 architecture rtl of counter is
 
 begin
-    process (A_in, clk) begin
-        if (rst = '1') then
-            count <= "00000000"; 
-        else 
-            if (clk'event and clk = '1' and ena = '1') then
-                count <= count_nxt;
-            end if;
-        end if;
-    end process;
+    with sel select B <=
+    A(7 downto 0) when "00000000",
+A(15 downto 8) when "00000001",
+A(23 downto 16) when "00000010"
+
 
 end rtl;
