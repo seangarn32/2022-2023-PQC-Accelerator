103ff679eef3485d1a4270b7061266b4625bcba1
Sam <scoulon@villanova.edu>
2022-09-27 21:55:21 -0400

    [WIP] Added sel and accum_ena signals

--- pqc_pipe/design/fsm.vhd
+++ pqc_pipe/design/fsm.vhd
@@ -10,9 +10,10 @@ entity fsm is
         rst : in  std_logic;
         ena : in  std_logic;
 
-        dsi_ena : out   std_logic;
-        pe_ena  : out   std_logic;
-        dso_ena : out   std_logic;
+        dsi_ena     : out   std_logic;
+        pe_ena      : out   std_logic;
+        accum_ena   : out   std_logic;
+        dso_ena     : out   std_logic;
 
         sel : out mux_sel_array
     );
@@ -20,84 +21,93 @@ end fsm;
 
 architecture rtl of fsm is
 
-    type state_available is (SETUP, DATA_IN, PE, DATA_OUT);  --type of state machine.
-    signal present_state: state_available;
+    type state_available is (SETUP, DATA_IN, PE_PIPE, DATA_OUT);  --type of state machine.
+    signal state            : state_available := SETUP;
+    signal state_nxt        : state_available;
     
-    signal counter : std_logic_vector(7 downto 0);
-    signal counter_ena : std_logic;
-    signal counter_reset : std_logic;
-    signal sel_hold : mux_sel_array:= (others =>(others => '0'));
-    signal sel_hold_nxt : mux_sel_array;
+    signal count            : std_logic_vector(7 downto 0);
+    signal counter_ena      : std_logic := '0';
+    signal counter_rst      : std_logic := '1';
+
+    signal sel_hold         : mux_sel_array := (others =>(others => '0'));
+    signal sel_nxt          : mux_sel_array;
 
 begin
 
     STATE_COUNTER: entity work.counter(rtl)
             port map (
-                clk, counter_reset, counter_ena, counter
+                clk, counter_rst, counter_ena, count
             );
 
-    process (clk, rst)
+    process (clk)
     begin
-        if falling_edge(clk) then
+
+        if rising_edge(clk) then
+
             if (rst='1') then
-                present_state <= SETUP;  --default state on reset.
-                counter_reset <= '0';
-            else 
+                state <= SETUP;  --default state on reset.
+                counter_rst <= '0';
 
-                case present_state is
+            else
+                case state_nxt is
 
-                    when SETUP =>        --when current state is "A"
-                        counter_reset <= '1';
-                        if(ena = '0') then
-                            present_state <= SETUP;
-                            dsi_ena <= '0';
-                            pe_ena <= '0';
-                            dso_ena <= '0';
-                        else
-                            present_state <= DATA_IN; -- once we flip a switch, it will move to DATA_IN
-                            dsi_ena <= '1';
-                        end if;  
+                    when SETUP =>
+                        dsi_ena <= '0';
+                        pe_ena <= '0';
+                        accum_ena <= '0';
+                        dso_ena <= '0';
+                        counter_ena <= '0';
+                        counter_rst <= '1';
 
-                    when DATA_IN =>        --when current state is "B"
+                        sel_hold <= (others =>(others => '0'));
+
+                    when DATA_IN =>
+                        dsi_ena <= '1';
                         counter_ena <= '1';
-                        counter_reset <= '0';
-                        if(counter = N_SIZE-1) then
-                            present_state <= PE;
-                            pe_ena <= '1';
-                            dsi_ena <= '0';
+                        counter_rst <= '0';
+
+                    when PE_PIPE =>
+                        dsi_ena <= '0';
+                        pe_ena <= '1';
+                        counter_ena <= '1';
+                        counter_rst <= '0';
+
+                        sel_hold <= sel_nxt;
+
+                        if(count > MUX_NUM-1) then
+                            accum_ena <= '1';
                         else
-                            present_state<= DATA_IN;
+                            accum_ena <= '0';
                         end if;
 
-                    when PE =>       --when current state is "C"
+                    when DATA_OUT =>
+                        pe_ena <= '0';
+                        accum_ena <= '0';
+                        dso_ena <= '1';
                         counter_ena <= '1';
-                        if(counter = N_SIZE-1) then
-                            present_state <= DATA_OUT;
-                            pe_ena <= '0';
-                            dso_ena <= '1';
-                        else
-                            present_state<= PE;
-                            sel_hold <= sel_hold_nxt;
-                        end if;
+                        counter_rst <= '0'; 
 
-                    when DATA_OUT =>         --when current state is "D"
-                        counter_ena <= '1';
-                        if(counter = N_SIZE-1) then
-                            present_state <= SETUP;
-                            dso_ena <= '0';
-                        else
-                            present_state<= DATA_OUT;
-                        end if;
-                        
                 end case;
+
+                state <= state_nxt;
+
             end if;
         end if;
     end process;
+
+    state_nxt <= SETUP      when rst = '1' or ena = '0'
+            else DATA_IN    when state = SETUP and rst = '0' and ena = '1'
+            else PE_PIPE    when state = DATA_IN and count = N_SIZE-1
+            else DATA_OUT   when state = PE_PIPE and count = MUX_NUM + DIVIDE - 1
+            else SETUP      when state = DATA_OUT and count = N_SIZE-1
+            else state;
     
+    sel_nxt(0) <= sel_hold(0) when sel_hold(0) = MUX_SIZE-1 
+                  else sel_hold(0) + '1';
     SEL_GEN : for i in 1 to MUX_NUM-1 generate
-        sel_hold_nxt(i) <= sel_hold(i-1);
+        sel_nxt(i) <= sel_hold(i-1);
     end generate SEL_GEN;
-    sel_hold_nxt(0) <= sel_hold(0) + '1' when sel_hold(0) = "1" else "1";
 
     sel <= sel_hold;
+
 end rtl;
