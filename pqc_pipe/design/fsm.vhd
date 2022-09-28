5e43400e2750610149cb45970138dabf2f446ea1
Sam <scoulon@villanova.edu>
2022-09-27 22:22:46 -0400

    [WIP] Almost working

--- pqc_pipe/design/fsm.vhd
+++ pqc_pipe/design/fsm.vhd
@@ -23,9 +23,8 @@ architecture rtl of fsm is
 
     type state_available is (SETUP, DATA_IN, PE_PIPE, DATA_OUT);  --type of state machine.
     signal state            : state_available := SETUP;
-    signal state_nxt        : state_available;
     
-    signal count            : std_logic_vector(7 downto 0);
+    signal count            : std_logic_vector(COUNTER_SIZE-1 downto 0);
     signal counter_ena      : std_logic := '0';
     signal counter_rst      : std_logic := '1';
 
@@ -45,11 +44,10 @@ begin
         if rising_edge(clk) then
 
             if (rst='1') then
-                state <= SETUP;  --default state on reset.
-                counter_rst <= '0';
+                state <= SETUP;
 
             else
-                case state_nxt is
+                case state is
 
                     when SETUP =>
                         dsi_ena <= '0';
@@ -61,20 +59,33 @@ begin
 
                         sel_hold <= (others =>(others => '0'));
 
+                        if(ena = '1') then
+                            state <= DATA_IN;
+                        end if;
+
                     when DATA_IN =>
                         dsi_ena <= '1';
                         counter_ena <= '1';
                         counter_rst <= '0';
 
+                        if(count = N_SIZE-1) then
+                            counter_rst <= '1';
+                            state <= PE_PIPE;
+                        end if;
+     
+
                     when PE_PIPE =>
                         dsi_ena <= '0';
                         pe_ena <= '1';
-                        counter_ena <= '1';
                         counter_rst <= '0';
 
                         sel_hold <= sel_nxt;
-
+                        
                         if(count > MUX_NUM-1) then
+                            if(count = MUX_NUM + DIVIDE - 1) then
+                                counter_rst <= '1';
+                                state <= DATA_OUT;
+                            end if;
                             accum_ena <= '1';
                         else
                             accum_ena <= '0';
@@ -84,23 +95,17 @@ begin
                         pe_ena <= '0';
                         accum_ena <= '0';
                         dso_ena <= '1';
-                        counter_ena <= '1';
-                        counter_rst <= '0'; 
+                        counter_rst <= '0';
+
+                        if(count = N_SIZE-1) then
+                            counter_rst <= '1';
+                            state <= SETUP;
+                        end if;
 
                 end case;
-
-                state <= state_nxt;
-
             end if;
         end if;
     end process;
-
-    state_nxt <= SETUP      when rst = '1' or ena = '0'
-            else DATA_IN    when state = SETUP and rst = '0' and ena = '1'
-            else PE_PIPE    when state = DATA_IN and count = N_SIZE-1
-            else DATA_OUT   when state = PE_PIPE and count = MUX_NUM + DIVIDE - 1
-            else SETUP      when state = DATA_OUT and count = N_SIZE-1
-            else state;
     
     sel_nxt(0) <= sel_hold(0) when sel_hold(0) = MUX_SIZE-1 
                   else sel_hold(0) + '1';
