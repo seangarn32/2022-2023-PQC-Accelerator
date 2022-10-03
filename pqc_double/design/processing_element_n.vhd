d4e0176d1421e6a60ca166382d1886b665cffe36
dsmth87 <dsmith87@villanova.edu>
2022-10-03 16:38:50 -0400

    [WIP] PE Input change

--- pqc_double/design/processing_element_n.vhd
+++ pqc_double/design/processing_element_n.vhd
@@ -9,11 +9,17 @@ entity processing_element_n is
         rst     : in    std_logic;
         ena     : in    std_logic;
         
-        A       : in    a_vector;
-        B       : in    std_logic_vector(7 downto 0);
-        C_in    : in    c_matrix;
+        A0      : in    a_vector;
+        A1      : in    a_vector;
 
-        C_out   : out   c_matrix
+        B0      : in    std_logic_vector(7 downto 0);
+        B1      : in    std_logic_vector(7 downto 0);
+
+        C_in0   : in    c_matrix;
+        C_in1   : in    c_matrix;
+
+        C_out0  : out   c_matrix;
+        C_out1  : out   c_matrix
     );
 end entity;
 
