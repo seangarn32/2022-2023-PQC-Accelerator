0dbb4fc4dd1a5b655f11a0beaebc4fd34840e38c
Robert Rotyliano <rrotylia@villanova.edu>
2022-10-25 18:14:35 -0400

    Recommitted double pe stuff

--- pqc_double/design/processing_element_i.vhd
+++ pqc_double/design/processing_element_i.vhd
@@ -8,41 +8,89 @@ entity processing_element_i is
         clk     : in    std_logic;
         rst     : in    std_logic;
         ena     : in    std_logic;
+        enc_dec : in    std_logic;
         
-        A0      : in    a_vector;
-        A1      : in    a_vector;
+        A0       : in    a_vector;
 
-        B0      : in    std_logic_vector(7 downto 0);
-        B1      : in    std_logic_vector(7 downto 0);
+        B_0       : in    std_logic_vector(7 downto 0);
+        B_1       : in    std_logic_vector(7 downto 0);
 
-        C_out0  : out   c_matrix;
-        C_out1  : out   c_matrix
+        C_out_0   : out   c_matrix
+        C_out_1   : out   c_matrix
+
+        A2     : out    a_vector;
     );
 end entity;
 
 architecture rtl of processing_element_i is
 
-    signal C_mult : c_matrix;
+    signal C_mult_0 : c_matrix;
+    signal C_mult_1 : c_matrix;
+    signal A1     : a_vector;
+    signal sc_A0  : a_vector;
+
 
 begin
     -- Multiply AxB -> C_mult
-    MULT :      entity work.multiplier_nbit(rtl)
+    MULT_0 :      entity work.multiplier_nbit(rtl)
         port map(
-            A,
-            B,
+            A0,
+            B_0,
 
-            C_mult
+            C_mult_0
         );
 
     -- Register Output -> C_out
-    REG_SUM :   entity work.reg_nbit_matrix(rtl)
+    REG_SUM_0 :   entity work.reg_nbit_matrix(rtl)
         port map(
             clk,
             rst,
             ena,
-            C_mult,
+            C_mult_0,
 
             C_out
+        );
+
+    -- Add code to circular shift A0 to A1.  If encryption, don't add sign
+    SHIFT_CELL:   entity work.shift_cell(rtl)
+        port map (
+            A0,
+
+            sc_A0
+        );
+
+    ENC_DEC_MUX:  entity work.enc_dec_mux(rtl)
+        port map (
+            sc_A0,
+            enc_dec,
+
+            A1
+        );
+
+    MULT_1 :      entity work.multiplier_nbit(rtl)
+        port map(
+            A1,
+            B_1,
+
+            C_mult_1
+        );
+
+    REG_SUM_1 :   entity work.reg_nbit_matrix(rtl)
+        port map(
+            clk,
+            rst,
+            ena,
+            C_mult_1,
+
+            C_out
+        );
+
+    -- Add code to circular shift A0 to A2
+    DOUBLE_SIGNED_CS : entity work.Dsigned_shift(rtl)
+        port map (
+            A0,
+
+            A2
         );
 
 end rtl;
