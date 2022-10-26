29fc175f9d971f07bcae64b992cca295da2e6010
Robert Rotyliano <rrotylia@villanova.edu>
2022-10-26 17:16:50 -0400

    Robs changes to pe_chain

--- pqc_double/design/pe_chain.vhd
+++ pqc_double/design/pe_chain.vhd
@@ -25,13 +25,23 @@ architecture rtl of pe_chain is
     signal a_wire       : a_array;
     signal b_wire       : b_matrix;
     signal p_wire       : b_matrix;
-    signal c_wire       : c_array;
+    signal c_wire_0       : c_array;
+    signal c_wire_1       : c_array;
 
 begin
 
     REG_B_GEN : for i in 1 to (PE_SIZE) generate 
 
-        B_REG : entity work.reg_8bit(rtl)
+        B_REG_0 : entity work.reg_8bit(rtl)
+            port map(
+                clk;
+                rst; 
+                ena;
+        
+                B_REG_IN;
+                B_REG_OUT;
+            );
+        B_REG_1 : entity work.reg_8bit(rtl)
             port map(
                 clk;
                 rst; 
@@ -60,27 +70,42 @@ begin
             c_wire(0)
         );
 
-    PE_I_GEN : for i in 1 to COLS-2 generate
+    PE_N_GEN : for i in 1 to COLS-2 generate
 
         type reg_link_i_wire is array (0 to i) of std_logic_vector(7 downto 0);
-        signal reg_link_i   : reg_link_i_wire := (others=>(others=>'0'));
+        signal reg_link_i_0   : reg_link_i_wire := (others=>(others=>'0'));
+        signal reg_link_i_1   : reg_link_i_wire := (others=>(others=>'0'));
 
     begin
 
-        reg_link_i(0) <= B(i);
+        reg_link_i_0(0) <= B0(i);
+        reg_link_i_1(0) <= B1(i);
 
-        REG_FEED_GEN_I : for j in 0 to i-1 generate
+        REG_FEED_GEN_I_0 : for j in 0 to i-1 generate
             REG_8 : entity work.reg_8bit(rtl)
                 port map(
                     clk,
                     rst,
                     ena,
 
-                    reg_link_i(j),
+                    reg_link_i_0(j),
 
-                    reg_link_i(j+1)
+                    reg_link_i_0(j+1)
                 );
-        end generate REG_FEED_GEN_I;
+        end generate REG_FEED_GEN_I_0;
+
+        REG_FEED_GEN_I_1 : for j in 0 to i-1 generate
+            REG_8 : entity work.reg_8bit(rtl)
+                port map(
+                    clk,
+                    rst,
+                    ena,
+
+                    reg_link_i_1(j),
+
+                    reg_link_i_1(j+1)
+                );
+        end generate REG_FEED_GEN_I_1;
 
         PE : entity work.processing_element_n(rtl)
             port map(
@@ -95,46 +120,46 @@ begin
                 a_wire(i+1),
                 c_wire(i)
             );
-    end generate PE_I_GEN;
-
-
-
-    PE_N_GEN : for i in 1 to 1 generate
-
-        type reg_link_n_wire is array (0 to COLS-1) of std_logic_vector(7 downto 0);
-        signal reg_link_n   : reg_link_n_wire := (others=>(others=>'0'));
-
-    begin
-
-        reg_link_n(0) <= B(COLS-1);
-
-        REG_FEED_GEN_N : for j in 0 to COLS-2 generate
-            REG_8 : entity work.reg_8bit(rtl)
-                port map(
-                    clk,
-                    rst,
-                    ena,
-
-                    reg_link_n(j),
-
-                    reg_link_n(j+1)
-                );
-        end generate REG_FEED_GEN_N;
-
-        PE_N :   entity work.processing_element_n(rtl)
-            port map(
-                clk,
-                rst,
-                ena,
-
-                a_wire(COLS-1),
-                reg_link_n(COLS-1),
-                c_wire(COLS-2),
-
-                c_wire(COLS-1)
-            );
-
     end generate PE_N_GEN;
+
+
+
+--    PE_N_GEN : for i in 1 to 1 generate
+--
+--        type reg_link_n_wire is array (0 to COLS-1) of std_logic_vector(7 downto 0);
+--        signal reg_link_n   : reg_link_n_wire := (others=>(others=>'0'));
+--
+--    begin
+--
+--        reg_link_n(0) <= B(COLS-1);
+--
+--        REG_FEED_GEN_N : for j in 0 to COLS-2 generate
+--            REG_8 : entity work.reg_8bit(rtl)
+--                port map(
+--                    clk,
+--                    rst,
+--                    ena,
+--
+--                    reg_link_n(j),
+--
+--                    reg_link_n(j+1)
+--                );
+--        end generate REG_FEED_GEN_N;
+--
+--        PE_N :   entity work.processing_element_n(rtl)
+--            port map(
+--                clk,
+--                rst,
+--                ena,
+--
+--                a_wire(COLS-1),
+--                reg_link_n(COLS-1),
+--                c_wire(COLS-2),
+--
+--                c_wire(COLS-1)
+--            );
+--
+--    end generate PE_N_GEN; 
 
     C_out <= c_wire(COLS-1);
 
