34a13cfbef14a36b42cb361ce199a82bff31b9a8
Robert Rotyliano <rrotylia@villanova.edu>
2022-11-04 12:05:30 -0400

    updated load_a w enc and dec

--- pqc_double/design/load_a.vhd
+++ pqc_double/design/load_a.vhd
@@ -23,27 +23,34 @@ architecture rtl of load_a is
     signal a2           : a_vector;
     signal tmp         : a_vector;
     signal a_reg         : a_vector;
+    signal a_init       : std_logic;
     
-    signal count        : std_logic_vector(N_SIZE/PE_SIZE downto 0) := (others => '0');
-    signal count_nxt    : std_logic_vector(N_SIZE/PE_SIZE downto 0) := (others => '0');
+    signal count        : std_logic := '0';
     
 begin
-    count <= count_nxt;
+    process(clk)
+    begin
+        if(rising_edge(clk)) then
+            if(rst = '1') then
+                a_init <= '0';
+            else 
+                if(pe_ena = '1') then
+                    a_init <= '1';
+                    if (count = '0') then
+                        count <= '1';
+                    else 
+                        count <= '0';
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
     
 
     -- Sign A_in to create A0
     SIGNED : for i in 0 to N_SIZE-1 generate
         a0(i) <= '0' & A_in(N_SIZE-1-i);
     end generate SIGNED;
-
-    a_nxt(0) <= a0;
-
-    --ENC_SHIFT_CELL_1:   entity work.signed_shift(rtl)
-    --port map (
-    --        a_nxt(i),
---
-    --        a_nxt(i + 1)
-    --    );
 
     ENC_DEC_SHIFT_CELL : for i in 0 to PE_SIZE*2 - 2 generate
         ENC_SHIFT_CELL_N: entity work.signed_shift(rtl)
@@ -54,10 +61,10 @@ begin
             );
     end generate ENC_DEC_SHIFT_CELL;
 
-    tmp <= a0 when (rst = '1' AND count /= (N_SIZE/PE_SIZE)) else
-             a_nxt(1) when (count /= (N_SIZE/PE_SIZE) AND enc_dec = '0') else
-             a_nxt(PE_SIZE*2 - 1) when ((count(0) = '0') AND count /= (N_SIZE/PE_SIZE) AND enc_dec = '1');
-             --a3 when (count mod 2 = '1' AND count /= (N_SIZE/PE_SIZE) AND enc_dec = '1');
+    tmp <= a0 when (rst = '1' and a_init = '0') else
+             a_nxt(1) when (enc_dec = '0' and a_init = '1' and count = '0') else
+             a_nxt(PE_SIZE * 2 - 1) when (enc_dec = '0' and a_init = '1' and count = '1') else
+             a_nxt(PE_SIZE * 2 - 1) when (enc_dec = '1' and a_init = '1');
 
     REG_A :   entity work.reg_nbit_a(rtl)
     port map(
@@ -68,8 +75,7 @@ begin
 
         a_reg
     );
-    
-    count_nxt <= count + '1';
+
     a_nxt(0) <= a_reg;
     A_out <= a_reg;
 
