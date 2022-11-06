b8e92f231e7712d4a0b6195095536c24727feb0a
dsmth87 <dsmith87@villanova.edu>
2022-11-05 23:24:39 -0400

    [WIP] inital load_b

---
+++ pqc_double/design/load_b.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.globals_pkg.all;
+
+entity load_b is
+    port(
+        clk         : in    std_logic;
+        rst         : in    std_logic;
+        load_b_ena  : in    std_logic;
+        enc_dec     : in    std_logic;
+
+        B_in        : in    b_matrix;
+        B_out       : out   b_matrix
+    );
+end entity;
+
+architecture rtl of load_b is
+
+
+ end rtl;
