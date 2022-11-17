9d77f7d5be6376ac1818c44e040d8581605a3e8d
Sean Garner <seangarn32@gmail.com>
2022-11-17 15:14:41 -0500

    Random mif maker

--- pqc_accelerator_new/Python Sims/Current Sims/mif_maker.py
+++ pqc_accelerator_new/Python Sims/Current Sims/mif_maker.py
@@ -1,8 +1,18 @@
-WIDTH = 8
-DEPTH = 256
-ADDRESS_RADIX = "UNS"
-DATA_RADIX = "UNS"
-FILEPATH = "test.mif"
+import random
+
+
+WIDTH = 8 #Same as the size of each B word, e.g. 8 bits
+MAXVAL = 2**(WIDTH-1) #Largest absolute value possible of signed width-size number
+DEPTH = 256 #Same as N
+ADDRESS_RADIX = "UNS" #Data format e.g. unsigned int
+DATA_RADIX = "UNS" #Data format e.g. unsigned int
+FILEPATH = "test.mif" #output filename
+
+#To generate A .mif
+#MEM = [random.randint(0, 1) for i in range(DEPTH)]
+
+#To generate B .mif
+MEM = [random.randint(-1*(MAXVAL),MAXVAL-1) for i in range(DEPTH)]
 
 file = open(FILEPATH, 'w')
 try:
@@ -11,7 +21,10 @@ try:
     file.write("ADDRESS_RADIX = "+ADDRESS_RADIX+";\n")
     file.write("DEPTH = "+DATA_RADIX+";\n\n")
     file.write("CONTENT BEGIN\n")
-    file.write("\t[0..255]  :   0;\n")
+    j = 0
+    for i in MEM:
+        file.write("\t["+str(j)+"]  :   "+str(i)+";\n")
+        j+=1
     file.write("END;\n")
 
 finally:
