47054607c287fa104fb6950e3343dd977323d5b4
dsmth87 <dsmith87@villanova.edu>
2022-12-04 18:40:23 -0500

    constant error fix

--- 2022-2023-PQC-Accelerator/pqc_double/design/const_error.vhd
+++ 2022-2023-PQC-Accelerator/pqc_double/design/const_error.vhd
@@ -8,18 +8,18 @@ entity const_error is
         clk     : in    std_logic;
         rst     : in    std_logic;
         ena     : in    std_logic;
+        enc_dec : in    std_logic;
 
         W       : in    std_logic_vector(7 downto 0);
         Y       : in    std_logic_vector(7 downto 0);
-        Z       : in    std_logic_vector(7 downto 0);
+        
         e1      : in    std_logic_vector(7 downto 0);
         e2      : in    std_logic_vector(7 downto 0);
         c2      : in    std_logic_vector(7 downto 0);
 
         Ci1      : out   std_logic_vector(7 downto 0);
-        Ci2      : out   std_logic_vector(7 downto 0);
-        D       : out   std_logic_vector(7 downto 0)
-    
+        Ci2      : out   std_logic_vector(7 downto 0)
+        
         );
 end const_error;
 
@@ -27,15 +27,18 @@ architecture rtl of const_error is
 
     signal sum_c1   : std_logic_vector(7 downto 0);
     signal sum_c2   : std_logic_vector(7 downto 0);
-    signal sum_d    : std_logic_vector(7 downto 0);
+    signal tmp      : std_logic_vector(7 downto 0);
 
 begin
 
-    sum_c1 <= W + e1;
-    sum_c2 <= Y + e2;
-    sum_d  <= Z + c2;
 
-    -- W + e1 (8-bit)
+    tmp <= e1 when (enc_dec = '0') else c2 when (enc_dec = '1');
+
+    sum_c1 <= W + tmp;
+
+    sum_c2 <= Y + e2;
+
+    -- W + e1 (8-bit) or Z + c2 (8-bit)
     REG_Ci1 : entity work.reg_8bit(rtl)
         port map(
             clk,
@@ -55,17 +58,6 @@ begin
 
             sum_c2,
             Ci2
-        );
-
-    -- Z + c2 (8-bit)
-    REG_D : entity work.reg_8bit(rtl)
-        port map(
-            clk,
-            rst,
-            ena,
-
-            sum_d,
-            D
         );
 
 end rtl;
