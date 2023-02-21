ddcfbc2a6a414370ce092d29fa074d78ff649c74
Robert Rotyliano <rrotylia@villanova.edu>
2023-02-20 22:35:04 -0500

    test files

--- 2022-2023-PQC-Accelerator/pqc_double/design/globals_pkg.vhd
+++ 2022-2023-PQC-Accelerator/pqc_double/design/globals_pkg.vhd
@@ -8,7 +8,7 @@ package globals_pkg is
     -- N_SIZE -> Size of initial A vector 
     constant N_SIZE : integer := 256;
     -- DIVIDE -> Number of times to divide down PEs for pipelining
-    constant DIVIDE : integer := 64;
+    constant DIVIDE : integer := 2;
     -- PE_SIZE -> Number of PEs used in design
     constant PE_SIZE : integer := N_SIZE/DIVIDE;
     
