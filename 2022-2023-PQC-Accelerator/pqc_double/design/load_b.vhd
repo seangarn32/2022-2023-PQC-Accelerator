2d7ca0a86d4602b68da1db0008bac9bb2b99e9f4
Robert Rotyliano <rrotylia@villanova.edu>
2023-03-18 19:15:03 -0400

    no latch b

--- 2022-2023-PQC-Accelerator/pqc_double/design/load_b.vhd
+++ 2022-2023-PQC-Accelerator/pqc_double/design/load_b.vhd
@@ -26,9 +26,10 @@ architecture rtl of load_b is
     signal p_even        :   b_hold_matrix;
     signal p_odd      :   b_hold_matrix;
     signal b_dec        : b_hold_matrix;
-    signal count      :  std_logic_vector (1 downto 0) := "00";
-    signal set_idx            :  integer := 0;
-    signal set_idx_hold            :  integer := 0;
+    signal count      :  std_logic := '0';
+    signal count_nxt     :  std_logic := '0';
+    signal set_idx        :  integer := 0;
+    signal set_idx_hold   :  integer := 0;
 
     begin
         --b_init <= '1' when (rst = '0' and load_b_ena = '1') else '0';
@@ -55,51 +56,59 @@ architecture rtl of load_b is
         --p_tmp <= (others => (others => '0')) when (rst = '1' and b_init = '0') else
         --    p_even when (enc_dec = '0' and b_init = '1' and count = "01") else
         --    p_odd when (enc_dec = '0' and b_init = '1' and count = "10");   
-    
-        B_out <= b_tmp;
-        P_out <= p_tmp;
+
+        process(load_b_ena, count)
+        begin
+            count_nxt <= count xor '1';
+            set_idx_hold <= set_idx + SET_SIZE;
+            if (load_b_ena = '1') then
+                if (enc_dec = '0') then
+                    if (count = '0') then
+                        set_idx_hold <= set_idx;
+                        --b_tmp <= b_even;
+                        --p_tmp <= p_even;
+                    --else
+                    --    b_tmp <= b_odd;
+                    --    p_tmp <= p_odd;
+                    end if;
+                --else
+                --    b_tmp <= b_dec;
+                end if;
+                if (set_idx = N_SIZE - SET_SIZE) then
+                    set_idx_hold <= set_idx;
+                end if;
+            end if;
+        end process;
 
         process(clk)
         begin
             if (rst = '1') then
                 b_tmp <= (others => (others => '0'));
                 p_tmp <= (others => (others => '0'));
-                --b_init <= '0';
-                count <= "00";
+                count <= '0';
                 set_idx <= 0;
-            else 
+            else
                 if (rising_edge(clk)) then
                     if (load_b_ena = '1') then
-                        set_idx <= set_idx;
-                        --b_init <= '1';
+                        count <= count_nxt;
+                        set_idx <= set_idx_hold;
                         if (enc_dec = '0') then
-                            if (count < "01") then
+                            if (count = '0') then
+                                --set_idx_hold <= set_idx;
                                 b_tmp <= b_even;
                                 p_tmp <= p_even;
-                                count <= count + "01";
-                            elsif (count = "01") then
+                            else
                                 b_tmp <= b_odd;
                                 p_tmp <= p_odd;
-                                count <= "00";
-                                if (set_idx < N_SIZE - SET_SIZE) then
-                                    set_idx <= set_idx + SET_SIZE;
-                                end if;
                             end if;
                         else
                             b_tmp <= b_dec;
-                            if (count <= "00") then
-                                count <= count + "01";
-                                if (set_idx < N_SIZE - SET_SIZE) then
-                                    set_idx <= set_idx + SET_SIZE;
-                                end if;
-                            else
-                                count <= "00";
-                            end if;
                         end if;
-                    else
-                        set_idx <= set_idx;
                     end if;
                 end if;
             end if;
         end process;
+
+        B_out <= b_tmp;
+        P_out <= p_tmp;
 end rtl;
